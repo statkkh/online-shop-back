aa552e1d6aae74ca3091d2126bd5cb8045461ac5
kkh <qr21@naver.com>
2023-12-22 18:08:14 +0900

    Feat  studyService controller : 관리자 controller에  POST Method 기능 추가
    관리자  에 대한 PostMapping에  경로 설정 및 postAdmin기능을  이용해 서비스 레이어 호출하여 성공일 떄 만 db 관리자 테이블 에 추가하기  및   결과(코드, 메시지)를  클라이언트에게 응답하기

--- src/main/java/com/online/shopping_back/repository/AdminRepository.java
+++ src/main/java/com/online/shopping_back/repository/AdminRepository.java
@@ -3,7 +3,6 @@ package com.online.shopping_back.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.mysql.cj.x.protobuf.MysqlxDatatypes.Scalar.String;
 import com.online.shopping_back.entity.AdminEntity;
 
 @Repository
