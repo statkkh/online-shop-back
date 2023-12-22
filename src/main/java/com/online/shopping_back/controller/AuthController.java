300e762edf168b921e07977e01f973921d377649
kkh <qr21@naver.com>
2023-12-22 13:07:43 +0900

    Feat :  Authcontroller : 로그인 하기 위한 controller에  POST Method  추가
    유저 정보에 대한 PostMapping에  경로 설정 및 signUp기능을  이용해 서비스 레이어 호출하여 성공일 떄 만 토큰 발급받기 및   실패와 성공에 대해서결과(코드, 메시지)를  클라이언트에게 응답하기**

--- src/main/java/com/online/shopping_back/controller/AuthController.java
+++ src/main/java/com/online/shopping_back/controller/AuthController.java
@@ -7,7 +7,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.online.shopping_back.dto.request.auth.SignInRequestDto;
 import com.online.shopping_back.dto.request.auth.SignUpRequestDto;
+import com.online.shopping_back.dto.response.auth.SignInResponseDto;
 import com.online.shopping_back.dto.response.auth.SignUpResponseDto;
 import com.online.shopping_back.service.AuthService;
 
@@ -28,4 +30,12 @@ public class AuthController {
         ResponseEntity<? super SignUpResponseDto> response = authService.signUp(requestBody);
         return response;
     }
+
+    @PostMapping("/sign-in")
+    public ResponseEntity<? super SignInResponseDto> signIn(
+        @RequestBody @Valid SignInRequestDto requestBody
+    ) {
+        ResponseEntity<? super SignInResponseDto> response = authService.signIn(requestBody);
+        return response;
+    }    
 }
