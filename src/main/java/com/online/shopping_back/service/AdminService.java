86fb7372e47cb62c81166b7122ba8e206a64e021
kkh <qr21@naver.com>
2023-12-23 12:08:55 +0900

    Feat : adminService(service && serviceImplement) 관리자 에   관한 정보를 읽기시 Controller의 응답에 대한 데이터 반환 , chore : adminServiceImplement : 매니저 존재 대한 검증 추가
    **관리자  읽기시  getAdmin()  이용한 Controller의 응답에 대한 데이터 비즈니스 로직에 따른 성공과 실패할 때 각각  응답에 대한 코드와 메시지 생성  성공시에는  db에 있는 관리자 테이블에서 관리자 이메일 에 해당되는 정보를 찾아 관리자 정보 가져오기**
    
    **adminServiceImplement : 관리자 정보 수정시  관리자 존재 대한 검증 추가**

--- src/main/java/com/online/shopping_back/service/AdminService.java
+++ src/main/java/com/online/shopping_back/service/AdminService.java
@@ -4,11 +4,13 @@ import org.springframework.http.ResponseEntity;
 
 import com.online.shopping_back.dto.request.admin.PatchAdminRequestDto;
 import com.online.shopping_back.dto.request.admin.PostAdminRequestDto;
+
+import com.online.shopping_back.dto.response.admin.GetAdminResponseDto;
 import com.online.shopping_back.dto.response.admin.PatchAdminResponseDto;
 import com.online.shopping_back.dto.response.admin.PostAdminResponseDto;
 
 public interface AdminService {
-
+    ResponseEntity<? super GetAdminResponseDto> getAdmin(String email);
     ResponseEntity<? super PostAdminResponseDto> postAdmin(PostAdminRequestDto dto,String email,  Integer userNumber);
     ResponseEntity<? super PatchAdminResponseDto> patchAdmin(PatchAdminRequestDto dto,String email);
 } 
