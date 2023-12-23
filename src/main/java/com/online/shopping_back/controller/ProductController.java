84f5f924fea1f9e4d1e83d19b2527214bee65b46
kkh <qr21@naver.com>
2023-12-23 16:38:18 +0900

    Feat :  ProductController: 상품정보 작성 하기 위한 controller에  POST Method  추가
    상품정보 에 대한 PostMapping에  경로 설정 및 postProduct기능을  이용해 서비스 레이어 호출하여 성공일 떄 만 DB에 추가 및 저장하기및   실패와 성공에 대해서결과(코드, 메시지)를  클라이언트에게 응답하기

--- src/main/java/com/online/shopping_back/controller/ProductController.java
+++ src/main/java/com/online/shopping_back/controller/ProductController.java
@@ -1,1 +1,39 @@
 package com.online.shopping_back.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.online.shopping_back.dto.request.product.PostProductRequestDto;
+import com.online.shopping_back.dto.response.product.PostProductResponseDto;
+import com.online.shopping_back.service.ProductService;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/product")
+@RequiredArgsConstructor
+public class ProductController {
+    
+    private final ProductService productService;
+
+    @PostMapping("/{userNumber}")
+    public ResponseEntity<? super PostProductResponseDto> postProduct(
+        @RequestBody @Valid PostProductRequestDto dto,
+        @AuthenticationPrincipal String managerEmail,
+        @PathVariable("userNumber") Integer userNumber
+    ){
+        ResponseEntity<? super PostProductResponseDto>  response = productService.postProduct(dto, managerEmail, userNumber);
+        return response;
+    }    
+}
