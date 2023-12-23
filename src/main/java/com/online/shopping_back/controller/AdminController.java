18876a057f0c49b14272964a41fe942b994ec977
kkh <qr21@naver.com>
2023-12-23 10:23:08 +0900

    Feat :  studyService controller  -스터디 자료 코멘트  에 관한정보를  작성하기 위한 controller에  PATCH Method  추가**  **chore :studyService controller - 관리자 작성에 대한 경로 수정**
    스터디 자료 코멘트 정보에 대한 PatchMapping에  경로 설정 및 patchMaterialComment기능을  이용해 서비스 레이어 호출하여 성공일 떄 만 db에 있는 스터디 자료 코멘트  테이블에 있는 데이터 수정하기 및   실패와 성공에 대해서결과(코드, 메시지)를  클라이언트에게 응답하기
    
    studyService controller - 관리자 작성에 대한 경로 수정
    
    @PostMapping("/{userNumber}")

--- src/main/java/com/online/shopping_back/controller/AdminController.java
+++ src/main/java/com/online/shopping_back/controller/AdminController.java
@@ -1,13 +1,17 @@
 package com.online.shopping_back.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.online.shopping_back.dto.request.admin.PatchAdminRequestDto;
 import com.online.shopping_back.dto.request.admin.PostAdminRequestDto;
+import com.online.shopping_back.dto.response.admin.PatchAdminResponseDto;
 import com.online.shopping_back.dto.response.admin.PostAdminResponseDto;
 import com.online.shopping_back.service.AdminService;
 
@@ -21,13 +25,22 @@ public class AdminController {
     
     private final AdminService adminService;
 
-    @PostMapping("/{email}/{userNumber}")
+    @PostMapping("/{userNumber}")
     public ResponseEntity<? super PostAdminResponseDto> postAdmin(
         @RequestBody @Valid PostAdminRequestDto dto,
-        @PathVariable("email") String email,
+        @AuthenticationPrincipal String email,
         @PathVariable("userNumber") Integer userNumber
     ){
         ResponseEntity<? super PostAdminResponseDto> response = adminService.postAdmin(dto, email, userNumber);
+        return response;
+    }
+
+    @PatchMapping("/")
+    public ResponseEntity<? super PatchAdminResponseDto> patchAdmin(
+        @RequestBody @Valid PatchAdminRequestDto dto,
+        @AuthenticationPrincipal String email
+    ){
+        ResponseEntity<?super PatchAdminResponseDto> response = adminService.patchAdmin(dto, email);
         return response;
     }
 }
