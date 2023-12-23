823cde0420ea1d37df2f0e18128f78eb6a8034a1
kkh <qr21@naver.com>
2023-12-23 16:24:16 +0900

    Feat  :  테이블과 객체를 연결및 데이터 변환및 post, patch시 데이터를 가져오는  Product Entity생성 , 데이터베이스에 접근및 작업을 위한  Product Repository 생성
    ProductEntity-
    ORM으로 DB 테이블과 객체 매핑및 요청 데이터및 필용한 데이터를 객체에 저장
    
    Product Repository -
     데이터 베이스 작업 기능 및  서비스 계층에 반환할 쿼리 메서드 생성

--- src/main/java/com/online/shopping_back/entity/ProductEntity.java
+++ src/main/java/com/online/shopping_back/entity/ProductEntity.java
@@ -7,12 +7,14 @@ import lombok.NoArgsConstructor;
 import java.util.Date;
 import java.text.SimpleDateFormat;
 import java.time.Instant;
+import java.time.LocalDateTime;
 
 import org.springframework.data.annotation.CreatedDate;
 
 import com.online.shopping_back.dto.request.product.PatchProductRequestDto;
 import com.online.shopping_back.dto.request.product.PostProductRequestDto;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -31,13 +33,14 @@ public class ProductEntity {
     private int userNumber;
     private String productName;
     private int price;
-    @CreatedDate
-    private String registerDate;
+    @Column(columnDefinition = "DATETIME DEFAULT CURRENT_TIMESTAMP")
+    private LocalDateTime   registerDate;
 
     public ProductEntity(PostProductRequestDto dto, Integer userNumber){        
         this.userNumber = userNumber;
         this.productName = dto.getProductName();
         this.price = dto.getPrice();
+        this.registerDate = LocalDateTime.now(); 
     }
 
     public void patchProduct(PatchProductRequestDto dto){
