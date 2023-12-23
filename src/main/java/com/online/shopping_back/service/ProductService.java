d1d7561256e124d3a5994cf45b0335d924854827
kkh <qr21@naver.com>
2023-12-23 17:41:09 +0900

    Feat : ProductService(service && serviceImplement) 상품 에   관한 정보를 수정시 Controller의 응답에 대한 데이터 반환
    상품 정보 수정 시  patchProduct()  이용한 Controller의 응답에 대한 데이터 비즈니스 로직에 따른 성공과 실패할 때 각각  응답에 대한 코드와 메시지 생성  성공시에는  db에 있는 상품 테이블에 수정 및 저장하기

--- src/main/java/com/online/shopping_back/service/ProductService.java
+++ src/main/java/com/online/shopping_back/service/ProductService.java
@@ -2,13 +2,14 @@ package com.online.shopping_back.service;
 
 import org.springframework.http.ResponseEntity;
 
+import com.online.shopping_back.dto.request.product.PatchProductRequestDto;
 import com.online.shopping_back.dto.request.product.PostProductRequestDto;
+import com.online.shopping_back.dto.response.product.PatchProductResponseDto;
 import com.online.shopping_back.dto.response.product.PostProductResponseDto;
 
 public interface ProductService {
 
     
     ResponseEntity<? super PostProductResponseDto> postProduct(PostProductRequestDto dto, String managerEmail, Integer userNumber);
-
-
+    ResponseEntity<? super PatchProductResponseDto> patchProduct(PatchProductRequestDto dto,String managerEmail, Integer userNumber);
 } 
