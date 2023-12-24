a0be6603c5667fdcf225f389b18c06c87f207ad1
kkh <qr21@naver.com>
2023-12-24 13:05:23 +0900

    Feat  :  PatchOrderProductResponseDto:응답 데이터를 전달하기 위한 데이터 전송 객체생성
    PatchStudyMaterialCommentRequestDto 서비스레이어에 응답에 대한 데이터및 코드와 메시지를 전달하기 위한 객체 생성

---
+++ src/main/java/com/online/shopping_back/dto/response/orderProduct/PatchOrderProductResponseDto.java
@@ -0,0 +1,43 @@
+package com.online.shopping_back.dto.response.orderProduct;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import com.online.shopping_back.dto.response.ResponseCode;
+import com.online.shopping_back.dto.response.ResponseDto;
+import com.online.shopping_back.dto.response.ResponseMessage;
+
+import lombok.Getter;
+
+@Getter
+public class PatchOrderProductResponseDto extends ResponseDto{
+    
+    private PatchOrderProductResponseDto(String code, String messsage){
+        super(code, messsage);
+    }
+
+    public static ResponseEntity<PatchOrderProductResponseDto> success(){
+        PatchOrderProductResponseDto result = new PatchOrderProductResponseDto(ResponseCode.SUCCESS, ResponseMessage.SUCCESS);
+        return ResponseEntity.status(HttpStatus.OK).body(result);
+    }
+
+    public static ResponseEntity<ResponseDto> notExistUser(){
+        ResponseDto result = new ResponseDto(ResponseCode.NOT_EXIST_USER, ResponseMessage.NOT_EXIST_USER);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(result);
+    }
+    
+    public static ResponseEntity<ResponseDto> notExistProduct(){
+        ResponseDto result = new ResponseDto(ResponseCode.NOT_FOUND_PRODUCT, ResponseMessage.NOT_FOUND_PRODUCT);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(result);
+    }    
+    
+    public static ResponseEntity<ResponseDto> notExistOrder(){
+        ResponseDto result = new ResponseDto(ResponseCode.NOT_EXIST_ORDER, ResponseMessage.NOT_EXIST_ORDER);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(result);
+    }     
+
+    public static ResponseEntity<ResponseDto> notExistOrderProduct(){
+        ResponseDto result = new ResponseDto(ResponseCode.NOT_FOUND_ORDER_PRODUCT, ResponseMessage.NOT_FOUND_ORDER_PRODUCT);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(result);
+    }        
+}
