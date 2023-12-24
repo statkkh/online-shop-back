9e586b1ca029876f6e3e3293f662b2174a86456c
kkh <qr21@naver.com>
2023-12-24 22:41:01 +0900

    Feat : BuyService(service && serviceImplement) 주문에   관한 정보를 patch시 Controller의 응답에 대한 데이터 반환
    주문 정보  수정시  patchBuy()  이용한 Controller의 응답에 대한 데이터 비즈니스 로직에 따른 성공과 실패할 때 각각  응답에 대한 코드와 메시지 생성  성공시에는  db에 있는  주문테이블에서 주문에 대한 정보가 수정 및 저장

--- src/main/java/com/online/shopping_back/service/implement/BuyServiceImplement.java
+++ src/main/java/com/online/shopping_back/service/implement/BuyServiceImplement.java
@@ -3,8 +3,10 @@ package com.online.shopping_back.service.implement;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import com.online.shopping_back.dto.request.order.PatchOrderRequestDto;
 import com.online.shopping_back.dto.request.order.PostOrderRequestDto;
 import com.online.shopping_back.dto.response.ResponseDto;
+import com.online.shopping_back.dto.response.order.PatchOrderResponseDto;
 import com.online.shopping_back.dto.response.order.PostOrderResponseDto;
 import com.online.shopping_back.dto.response.orderProduct.PostOrderProductResponseDto;
 import com.online.shopping_back.entity.BuyEntity;
@@ -42,6 +44,26 @@ public class BuyServiceImplement implements BuyService{
             return ResponseDto.databaseError();
         }
         return PostOrderResponseDto.success();
+    }
+
+    @Override
+    public ResponseEntity<? super PatchOrderResponseDto> patchBuy(PatchOrderRequestDto dto, String email,Integer userNumber) {
+        
+        try {
+            boolean existedUser = userRepository.existsByEmail(email);
+            if(!existedUser) return PatchOrderResponseDto.notExistUser();
+            
+            BuyEntity buyEntity = buyRepository.findByOrderNumber(dto.getOrderNumber());
+            if(buyEntity == null) return PatchOrderResponseDto.notExistOrder();
+
+            buyEntity.patchBuy(dto);
+            buyRepository.save(buyEntity);
+
+        } catch (Exception exception) {
+            exception.printStackTrace();
+            return ResponseDto.databaseError();
+        }
+        return PatchOrderResponseDto.success();
     }
     
 }
