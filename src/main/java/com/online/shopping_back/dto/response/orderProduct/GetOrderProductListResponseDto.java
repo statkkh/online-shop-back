89b451b50846aa6419621777331ca36004b1d153
kkh <qr21@naver.com>
2023-12-24 17:48:48 +0900

    chore :  GetOrderProductListResponseDto 응답 데이터를 전달하기 위한 데이터 전송 객체생성
    비즈니스 로직에 실패에 대해 주문상품이 없음에 대한 코드와 메시지 생성

--- src/main/java/com/online/shopping_back/dto/response/orderProduct/GetOrderProductListResponseDto.java
+++ src/main/java/com/online/shopping_back/dto/response/orderProduct/GetOrderProductListResponseDto.java
@@ -29,5 +29,23 @@ public class GetOrderProductListResponseDto extends ResponseDto{
         return ResponseEntity.status(HttpStatus.OK).body(result);
     }
 
+    public static ResponseEntity<ResponseDto> notExistUser(){
+        ResponseDto result = new ResponseDto(ResponseCode.NOT_EXIST_USER, ResponseMessage.NOT_EXIST_USER);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(result);
+    }
+
+    public static ResponseEntity<ResponseDto> notExistManager(){
+        ResponseDto result = new ResponseDto(ResponseCode.NOT_EXIST_MANAGER, ResponseMessage.NOT_EXIST_MANAGER);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(result);
+    }   
     
+    public static ResponseEntity<ResponseDto> notExistProduct(){
+        ResponseDto result = new ResponseDto(ResponseCode.NOT_FOUND_PRODUCT, ResponseMessage.NOT_FOUND_PRODUCT);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(result);
+    }    
+    
+    public static ResponseEntity<ResponseDto> notExistOrder(){
+        ResponseDto result = new ResponseDto(ResponseCode.NOT_EXIST_ORDER, ResponseMessage.NOT_EXIST_ORDER);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(result);
+    }         
 }
