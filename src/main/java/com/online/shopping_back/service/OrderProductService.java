8e204c52648ba5c789c19de772d7ad3df208a7b7
kkh <qr21@naver.com>
2023-12-24 13:07:00 +0900

    Feat  : OrderProductService(service && serviceImplement) 주문상품에   관한 정보를 PATCH 때 Controller의 응답에 대한 데이터 반환
    주문 상품 작성시  patchOrderProduct()  이용한 Controller의 응답에 대한 데이터 비즈니스 로직에 따른 실패할 떄 각각  응답에 대한 코드와 메시지 생성  성공할 때는  db에서 주문 상품 번호를 찾아 수정  및 저장

--- src/main/java/com/online/shopping_back/service/OrderProductService.java
+++ src/main/java/com/online/shopping_back/service/OrderProductService.java
@@ -2,13 +2,16 @@ package com.online.shopping_back.service;
 
 import org.springframework.http.ResponseEntity;
 
+import com.online.shopping_back.dto.request.orderProduct.PatchOrderProductRequestDto;
 import com.online.shopping_back.dto.request.orderProduct.PostOrderProductRequestDto;
 import com.online.shopping_back.dto.response.orderProduct.PostOrderProductResponseDto;
+
+import com.online.shopping_back.dto.response.orderProduct.PatchOrderProductResponseDto;
 
 public interface OrderProductService {
 
     
     ResponseEntity<? super PostOrderProductResponseDto>postOrderProduct(PostOrderProductRequestDto dto, String email, Integer userNumber, Integer orderNumber,Integer productNumber);
-
+    ResponseEntity<? super PatchOrderProductResponseDto> patchOrderProduct(PatchOrderProductRequestDto dto, String email, Integer userNumber, Integer orderNumber,Integer productNumber);
 
 } 
