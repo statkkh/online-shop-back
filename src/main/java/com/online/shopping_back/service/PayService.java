173b4ca4c56fa16c40a590bc695b063d5e3edea8
kkh <qr21@naver.com>
2023-12-26 12:32:43 +0900

    Feat : PayService(service && serviceImplement) 결제 에   관한 정보를 수정시 Controller의 응답에 대한 데이터 반환
    결제 정보 수정시  patchPay()  이용한 Controller의 응답에 대한 비즈니스 로직에 따른 성공과 실패할 때 각각  응답에 대한 코드와 메시지 생성  성공시에는  db에 있는 결제  테이블 수정

--- src/main/java/com/online/shopping_back/service/PayService.java
+++ src/main/java/com/online/shopping_back/service/PayService.java
@@ -2,11 +2,13 @@ package com.online.shopping_back.service;
 
 import org.springframework.http.ResponseEntity;
 
+import com.online.shopping_back.dto.request.pay.PatchPayRequestDto;
 import com.online.shopping_back.dto.request.pay.PostPayRequestDto;
+import com.online.shopping_back.dto.response.pay.PatchPayResponseDto;
 import com.online.shopping_back.dto.response.pay.PostPayResponseDto;
 
 public interface PayService {
     
     ResponseEntity<? super  PostPayResponseDto> postPay(PostPayRequestDto dto,String email,Integer userNumber, Integer orderNumber);
-    
+    ResponseEntity<? super PatchPayResponseDto> patchPay(PatchPayRequestDto dto, String email,Integer userNumber, Integer orderNumber);        
 }
