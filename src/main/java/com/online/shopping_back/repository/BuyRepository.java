0d4e76c157ae978991443db0dbe11c1c1f56ac7e
kkh <qr21@naver.com>
2023-12-26 09:31:26 +0900

    Feat  :  데이터베이스(주문  정보)에 접근및 작업을 위한  OrderRepository생성
    OrderRepository --
    데이터 베이스(주문  정보) 작업 기능 생성및 서비스 계층에 반환할 쿼리 메서드 생성

--- src/main/java/com/online/shopping_back/repository/BuyRepository.java
+++ src/main/java/com/online/shopping_back/repository/BuyRepository.java
@@ -1,6 +1,7 @@
 package com.online.shopping_back.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.online.shopping_back.entity.BuyEntity;
@@ -12,5 +13,23 @@ public interface BuyRepository extends JpaRepository<BuyEntity, Integer>{
     
     boolean existsByOrderNumber(Integer orderNumber);
 
-    List<BuyEntity> findByUserNumber(Integer userNumber);
+    // @Query(
+    //     value = 
+    //     "SELECT " +
+    //         "B.user_number AS userNumber, " +
+    //         "B.product_name AS productName, " +
+    //         "B.order_datetime AS orderDatetime, " +
+    //         "(  " +
+    //             "SELECT SUM(OP2.product_price) " +
+    //             "FROM order_product  AS OP2 " +
+    //             "WHERE B.order_number = OP2.order_number " +
+    //             "AND B.product_name = OP2.product_name " +
+    //         ") AS orderTotalPrice " +
+    //     "FROM buy AS B " +
+    //     "JOIN order_product AS OP " +
+    //     "ON B.product_name = OP.product_name " ,
+    //     nativeQuery = true
+    // )
+    
+    BuyEntity findByOrderNumber(Integer orderNumber);
 }
